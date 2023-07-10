ad3b228d0b5e26f5a5e174e2352a5c82245270b6
Ionut <81566145+Blitz-Cloud@users.noreply.github.com>
2023-07-10 19:53:22 +0300

    Fix back svg on /login

--- site/client/src/routes/Login.jsx
+++ site/client/src/routes/Login.jsx
@@ -10,7 +10,7 @@ export default function Login() {
         className="items center m-2 flex h-[430px] max-w-[340px] flex-col 
       items-center  overflow-hidden rounded-lg  bg-white/20 p-4 text-center text-black backdrop-blur-md"
       >
-        <ArrowBack className="m-1 h-auto w-[24px] self-start rounded-full fill-black hover:bg-black/20" />
+        <ArrowBack className="m-1 h-auto w-[24px] self-start rounded-full fill-black hover:bg-black/20 dark:fill-white" />
         <div className="mb-32 mt-4">
           <h1 className="mb-2 text-5xl font-bold text-[#3a0ca3] dark:text-white">
             Login
