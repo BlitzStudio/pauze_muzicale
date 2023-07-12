09437fbff593cb0220c6e546129fe5b43878486c
Ionut <81566145+Blitz-Cloud@users.noreply.github.com>
2023-07-12 15:07:14 +0300

    Update added new svg

---
+++ site/client/src/svgs/Done.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function FormSvg({ className }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -960 960 960"
+      className={className}
+    >
+      <path d="M378-246 154-470l43-43 181 181 384-384 43 43-427 427Z" />
+    </svg>
+  );
+}
