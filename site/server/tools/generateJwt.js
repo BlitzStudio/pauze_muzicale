5bcebb589e3bcbd76ca7411c622c7f57373539da
Ionut <81566145+Blitz-Cloud@users.noreply.github.com>
2023-07-13 21:45:59 +0300

    Fix

--- site/server/tools/generateJwt.js
+++ site/server/tools/generateJwt.js
@@ -4,14 +4,14 @@ const ACCESS_SECRET = process.env["JWT_ACCESS_SECRET"]
 const REFRESH_SECRET = process.env["JWT_REFRESH_SECRET"]
 
 export default function generateJwtPair(user) {
-    const currentTime = Date.now()
+    const currentTime = (Date.now()) / 1000
     const token = {
         email: user.email,
         name: user.name,
         picture: user.picture,
         iat: currentTime,
         // expira intr o zi
-        exp: currentTime + 24 * 60 * 60 * 1000,
+        exp: currentTime + 24 * 60 * 60,
         role: user.role,
         azp: user._id,
         submittedCount: user.submittedCount,
@@ -20,7 +20,7 @@ export default function generateJwtPair(user) {
     }
     const accessToken = jwt.sign(token, ACCESS_SECRET)
     // expira dupa o saptamana de la initiere
-    token.exp = currentTime + 7 * 24 * 60 * 60 * 1000
+    token.exp = currentTime + 7 * 24 * 60 * 60
     const refreshToken = jwt.sign(token, REFRESH_SECRET)
     return { accessToken, refreshToken }
 }
