fc0b5fe7608041e165ab75bc75a392daaa33a772
Ionut <81566145+Blitz-Cloud@users.noreply.github.com>
2023-07-20 18:39:53 +0300

    Fixes

--- site/server/routes/utils.js
+++ site/server/routes/utils.js
@@ -1,49 +1,57 @@
-import { Router } from 'express'
-import jwt from 'jsonwebtoken'
-import { AsyncHandler, CustomError, errorHandler } from '../middleware/errorHandler.js'
-import isAuth from '../middleware/isAuth.js'
-import User from '../models/user.js'
-import Music from '../models/musicTrack.js'
-import generateJwtPair from '../tools/generateJwt.js'
-const router = Router()
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+import {
+  AsyncHandler,
+  CustomError,
+  errorHandler,
+} from "../middleware/errorHandler.js";
+import isAuth from "../middleware/isAuth.js";
+import User from "../models/user.js";
+import Music from "../models/musicTrack.js";
+import generateJwtPair from "../tools/generateJwt.js";
 
-const ACCESS_SECRET = process.env["JWT_ACCESS_SECRET"]
-const REFRESH_SECRET = process.env["JWT_REFRESH_SECRET"]
+const router = Router();
 
+const ACCESS_SECRET = process.env["JWT_ACCESS_SECRET"];
+const REFRESH_SECRET = process.env["JWT_REFRESH_SECRET"];
 
-
-
-router.post("/songs", isAuth, AsyncHandler(async (req, res) => {
-    const { ids } = req.body
+router.post(
+  "/songs",
+  isAuth,
+  AsyncHandler(async (req, res) => {
+    const { ids } = req.body;
     // console.log(req.headers.authorization.split(" ")[1])
-    ids.forEach(async id => {
-        const db = await Music.findById(id)
-        if (!db) {
-            const track = new Music({
-                _id: id,
-            })
-            await track.save()
-        }
+    ids.forEach(async (id) => {
+      const db = await Music.findOne({ ytId: id });
+      if (!db) {
+        const track = new Music({
+          ytId: id,
+        });
+        await track.save();
+      }
     });
-    const user = await User.findById(req.user.azp)
-    const now = new Date()
-    const refreshWindow = new Date(user.refreshWindow)
-    console.log(!user.submittedCount || (user.submittedCount == 3 && refreshWindow < now));
-    if (!user.submittedCount || (user.submittedCount == 3 && refreshWindow < now)) {
-        now.setHours(0)
-        now.setMinutes(0)
-        now.setSeconds(0)
-        now.setMilliseconds(0)
-        user.refreshWindow = now.getTime() + 7 * 24 * 60 * 60 * 1000
-        user.submittedCount = ids.length
+    const user = await User.findById(req.user.azp);
+    const now = new Date();
+    const refreshWindow = new Date(user.refreshWindow);
+    if (
+      !user.submittedCount ||
+      (user.submittedCount == 3 && refreshWindow < now)
+    ) {
+      now.setHours(0);
+      now.setMinutes(0);
+      now.setSeconds(0);
+      now.setMilliseconds(0);
+      user.refreshWindow = now.getTime() + 7 * 24 * 60 * 60 * 1000;
+      user.submittedCount = ids.length;
     } else {
-        user.submittedCount += ids.length
+      user.submittedCount += ids.length;
     }
-    const { accessToken, refreshToken } = generateJwtPair(user)
-    user.refreshToken = refreshToken
-    await user.save()
+    const { accessToken, refreshToken } = generateJwtPair(user);
+    user.refreshToken = refreshToken;
+    await user.save();
 
-    res.send(accessToken)
-}))
+    res.send(accessToken);
+  })
+);
 
-export default router
+export default router;
